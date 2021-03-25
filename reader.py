89b09fb961ffc8a04c98166d28cdd558cc0c02ae
davidzhang <david.j.zhang@vanderbilt.edu>
2021-03-25 14:12:14 -0500

    bug fixes after testing session

--- reader.py
+++ reader.py
@@ -13,7 +13,7 @@ import math
 location = "Senior Design Lab"
 
 # COM port which RFID reader module is plugged into
-COM_PORT_NAME = "COM4"
+COM_PORT_NAME = "/dev/ttyUSB0"
 
 # connect to reader using pyrfidgeek package
 reader = PyRFIDGeek(serial_port=COM_PORT_NAME, debug=True)
@@ -22,7 +22,8 @@ reader = PyRFIDGeek(serial_port=COM_PORT_NAME, debug=True)
 protocols = [ISO15693]
 
 # connect to database
-myclient = pymongo.MongoClient("mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.muah1.mongodb.net/rfivDB?retryWrites=true&w=majority")
+myclient = pymongo.MongoClient(
+    "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.muah1.mongodb.net/rfivDB?retryWrites=true&w=majority")
 mydb = myclient["rfivDB"]
 mycol = mydb["patients"]
 
@@ -44,27 +45,35 @@ try:
         # save locations into patient database entries that are associated with the tag IDs
         for tagId in tagIds:
             # get the patient entry with this tag ID
+            # the tag ID is stored as little-endian, so it must be reversed
+            # reversing trick is adapted from John La Rooy at https://stackoverflow.com/a/5864372
             patient = mycol.find_one({
-                "tagId": tagId[:-1][::-1]
+                "tagId": "".join(map(str.__add__, tagId[-2::-2], tagId[-1::-2]))
             })
 
-            # get the last location/time
-            lastLocation = patient["locations"][-1]
+            # if the tag is associated with a patient, save to database
+            if patient is not None:
+                # get the last location/time if it exists
+                if len(patient["locations"]) == 0:
+                    lastLocation = [0, ""]
+                else:
+                    lastLocation = patient["locations"][-1]
 
-            # if the current location is different from the last location, add it to the database
-            # or if it's been over 120 seconds regardless of the location, add it to the database
-            # otherwise, don't add it to save space in database
-            if location != lastLocation[1] or timestamp - lastLocation[0] > 120000:
-                mycol.update_one({
-                    "_id": patient["_id"]
-                }, {
-                    "$push": {
-                        "locations": [timestamp, location]
-                    }
-                })
-                print(f"Saved location for {patient['name']} at {timestamp}")
-            else:
-                print("This tag was recently read in this location.")
+                # if the current location is different from the last location, add it to the database
+                # or if it's been over 120 seconds regardless of the location, add it to the database
+                # otherwise, don't add it to save space in database
+                if location != lastLocation[1] or timestamp - lastLocation[0] > 120000:
+                    mycol.update_one({
+                        "_id": patient["_id"]
+                    }, {
+                        "$push": {
+                            "locations": [timestamp, location]
+                        }
+                    })
+                    print(
+                        f"Saved location for {patient['name']} at {timestamp}")
+                else:
+                    print("This tag was recently read in this location.")
 
         # read every half second
         time.sleep(0.5)
